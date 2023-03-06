c37bdbbfe62bab064f7bb2ac45d776fa80a33990
KuzminaSofia <sivanstiven@gmail.com>
2023-03-06 20:43:52 +0300

    Add project files.

---
+++ KuzminaSA/Controllers/HelloWorldController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace KuzminaSA.Controllers;
+
+public class HelloWorldController : Controller
+{
+    // 
+    // GET: /HelloWorld/
+    //public string Index()
+    //{
+    //    return "This is my default action...";
+    //}
+    public IActionResult Index()
+    {
+        return View();
+    }
+    // 
+    public IActionResult Welcome(string name, int numTimes = 1)
+    {
+        ViewData["Message"] = "Hello " + name;
+        ViewData["NumTimes"] = numTimes;
+        return View();
+    }
+}
---
+++ KuzminaSA/Data/KuzminaSAContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using KuzminaSA.Models;
+
+namespace KuzminaSA.Data
+{
+    public class KuzminaSAContext : DbContext
+    {
+        public KuzminaSAContext (DbContextOptions<KuzminaSAContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<KuzminaSA.Models.Movie> Movie { get; set; } = default!;
+    }
+}
